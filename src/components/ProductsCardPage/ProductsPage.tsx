9e17191eea740eae19f2fe657172d8c2bdb2cdc3
rjy14 <ryan.james54@hotmail.com>
2023-11-10 16:36:54 +0700

    SearchFunction

--- src/components/ProductsCardPage/ProductsPage.tsx
+++ src/components/ProductsCardPage/ProductsPage.tsx
@@ -1,12 +1,12 @@
-import Card from 'react-bootstrap/Card';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import React, { useState } from 'react';
-import "../../pages/HomePage/Homepage.css"
-import jsonData from '../../constant/data.json';
-import Pagination from '../Pagination/Index';
-import { Link} from 'react-router-dom';
-import { useShoppingCart } from '../../context/CartContext';
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import React, { useState } from "react";
+import "../../pages/HomePage/Homepage.css";
+import jsonData from "../../constant/data.json";
+import Pagination from "../Pagination/Index";
+import { Link } from "react-router-dom";
+import { useShoppingCart } from "../../context/CartContext";
 function ProjectsPage() {
   const {
     increaseCartQuantity,
@@ -14,56 +14,91 @@ function ProjectsPage() {
     // removeFromCart,
   } = useShoppingCart();
   const [currentPage, setCurrentPage] = useState<number>(1);
+  const [searchKeyword, setSearchKeyword] = useState<string>("");
   const lastPage: number = 20;
   const cardsPerPage: number = 12;
   const indexOfLastCard: number = currentPage * cardsPerPage;
   const indexOfFirstCard: number = indexOfLastCard - cardsPerPage;
-  const currentCards = jsonData.slice(indexOfFirstCard, indexOfLastCard);
-  // const handlePageChange = (page: number) => {
-  //   setCurrentPage(page);
-  // };
-  const imageSize: React.CSSProperties = {
-    width: "155px", // Set the width of the image
-    objectFit: "cover", // Set the height of the image
-    // Set the position of the
+  // Filter cards, from keywords that user entered
+  const filteredCards = jsonData.filter((product) =>
+    product.Product_name.toLowerCase().includes(searchKeyword.toLowerCase())
+  );
+  const currentCards = filteredCards.slice(indexOfFirstCard, indexOfLastCard);
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchKeyword(event.target.value);
+    setCurrentPage(1); // Reset page to 1 when search keyword changes
   };
+  const imageSize: React.CSSProperties = {
+    width: "155px",
+    objectFit: "cover",
+};
   return (
     <>
-      <div className="scrollable-table">
-        <br></br>
-        <Row xs={1} md={4} className="g-0 justify-content-center">
-          {currentCards.map((Product, id) => (
-            <Col key={id} className="p-2 d-flex justify-content-center">
-              <Card>
-                <Link to={`/product/${Product.Product_id}`}>
-                  <div className="text-center">
-                    <Card.Img src={Product.Product_picture} style={imageSize} />
-                  </div>
-                  <Card.Body>
-                    <Card.Title>{Product.Product_name}</Card.Title>
-                    <Card.Text>{Product.Product_price}</Card.Text>
-                  </Card.Body>
-                </Link>
-                <Card.Footer className="card-footer">
-                  <button
-                    className="button"
-                    onClick={() => increaseCartQuantity(Product.Product_id)}
-                  >
-                    Add to cart
-                  </button>
-                </Card.Footer>
-              </Card>
-            </Col>
-          ))}
-        </Row>
-      </div>
-      <div>
-        <Pagination
-          currentPage={currentPage}
-          lastPage={lastPage}
-          maxLength={7}
-          setCurrentPage={setCurrentPage}
+      <h1>HighFashion</h1>
+      <div className="search-box">
+        <input
+          type="text"
+          placeholder="Search..."
+          value={searchKeyword}
+          onChange={handleSearchChange}
+          style={{
+            borderRadius: "90px",
+            border: "1px solid #ccc",
+            width: "280px",
+          }}
         />
+      </div>
+
+      <div className="filtering-contents">
+        <div className="filter"> 
+        </div>
+      </div>
+      
+      <div className="product-contents">
+      <div className="filter"> 
+        </div>
+        <div className="scrollable-table">
+          <Row xs={1} md={4} className="g-0 justify-content-center">
+            {currentCards.map((Product, id) => (
+              <Col key={id} className="p-2 d-flex justify-content-center">
+                <Card>
+                  <Link to={`/product/${Product.Product_id}`}>
+                    <div className="text-center">
+                      <Card.Img
+                        src={Product.Product_picture}
+                        style={imageSize}
+                      />
+                    </div>
+                    <Card.Body>
+                      <Card.Title style={{ color: "black" }}>
+                        {Product.Product_name}
+                      </Card.Title>
+                      <Card.Text style={{ color: "black" }}>
+                        {Product.Product_price}
+                      </Card.Text>
+                    </Card.Body>
+                  </Link>
+                  <Card.Footer className="card-footer">
+                    <button
+                      className="button"
+                      onClick={() => increaseCartQuantity(Product.Product_id)}
+                    >
+                      Add to cart
+                    </button>
+                  </Card.Footer>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </div>
+        <div>
+          <Pagination
+            currentPage={currentPage}
+            lastPage={lastPage}
+            maxLength={7}
+            setCurrentPage={setCurrentPage}
+          />
+        </div>
       </div>
     </>
   );
